b671674f80bbee3bcfb6dbccc961caee7916d466
ymzkjpx <ymzkjpx@gmail.com>
2021-08-30 13:22:07 +0900

    first commit

---
+++ src/main/java/com/cloneisolation/CloneIsolationApplication.java
@@ -0,0 +1,13 @@
+package com.cloneisolation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneIsolationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneIsolationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cloneisolation/CloneIsolationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloneisolation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloneIsolationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
