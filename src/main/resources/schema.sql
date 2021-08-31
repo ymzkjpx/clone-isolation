009539948ba7df1edb73b6d23693733d5a950bfd
ymzkjpx <ymzkjpx@gmail.com>
2021-08-31 09:13:37 +0900

    テーブルを作成

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,44 +1,25 @@
-DROP TABLE IF EXISTS employees;
+DROP SCHEMA IF EXISTS 給与 CASCADE;
+CREATE SCHEMA 給与;
 
-DROP SCHEMA IF EXISTS account CASCADE;
-CREATE SCHEMA account;
-DROP SEQUENCE IF EXISTS account.employee_id;
-CREATE SEQUENCE account.employee_id;
-
-CREATE TABLE IF NOT EXISTS account.employees
+CREATE SEQUENCE 給与.従業員ID;
+CREATE TABLE IF NOT EXISTS 給与.従業員
 (
-    employee_id INTEGER PRIMARY KEY ,
-    name VARCHAR(60) NOT NULL ,
-    hourly_wage DOUBLE PRECISION DEFAULT 0.0 ,
-    registered_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    従業員ID INTEGER PRIMARY KEY ,
+    登録日時 TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO
-    account.employees(employee_id ,name ,hourly_wage)
-VALUES
-    (1 ,'Alice' ,900)
-  , (2 ,'Bob' ,950)
-  , (3 ,'Carol' ,1000)
-  , (4 ,'Eve' ,NULL)
-;
-
-DROP TABLE IF EXISTS timecards;
-DROP SCHEMA IF EXISTS attendance CASCADE;
-CREATE SCHEMA attendance;
-DROP SEQUENCE IF EXISTS attendance.timecard_id;
-CREATE SEQUENCE attendance.timecard_id;
-
-CREATE TABLE IF NOT EXISTS attendance.timecards
+CREATE SEQUENCE 給与.就業時間ID;
+CREATE TABLE IF NOT EXISTS 給与.就業時間
 (
-    timecard_id INTEGER PRIMARY KEY ,
-    employee_id INTEGER ,
-    registered_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
-    FOREIGN KEY (employee_id) REFERENCES account.employees (employee_id)
-);
-
-DROP TABLE IF EXISTS jobs;
-CREATE TABLE attendance.jobs
-(
-    timecard_id INTEGER NOT NULL ,
-    start_time TIMESTAMP DEFAULT current_timestamp
+    従業員ID INTEGER NOT NULL ,
+    就業時間ID INTEGER NOT NULL ,
+    勤務日 DATE NOT NULL ,
+    開始日時 TIMESTAMP NOT NULL ,
+    終了日時 TIMESTAMP NOT NULL ,
+    休憩時間 INTEGER NOT NULL ,
+    深夜休憩時間 INTEGER NOT NULL ,
+    休日 VARCHAR(3) NOT NULL ,
+    登録日時 TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+    PRIMARY KEY (従業員ID ,勤務日) ,
+    FOREIGN KEY (従業員ID) REFERENCES 給与.従業員 (従業員ID)
 );
